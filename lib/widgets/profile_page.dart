104309d6bb89d44339aea041f41eefa22b1190e1
Reema Alghanmi <reemaalghanmii@gmail.com>
2024-04-25 17:34:59 +0300

    27th commit

--- lib/widgets/profile_page.dart
+++ lib/widgets/profile_page.dart
@@ -161,22 +161,42 @@ class UserPage extends StatelessWidget {
               onPressed: () {
                 Navigator.push(
                   context,
+                  MaterialPageRoute(builder: (context) => ChangeEmailPage()),
+                );
+              },
+              child: Text(
+                '  Change Email  ',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF009688),
+                padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 14.0),
+                textStyle: TextStyle(fontSize: 16.0),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
                   MaterialPageRoute(builder: (context) => PasswordResetPage()),
                 );
               },
-              child: Text('Reset Password',
-              style: TextStyle(
-                    color: Colors.white,
-                  ),
+              child: Text(
+                'Reset Password',
+                style: TextStyle(
+                  color: Colors.white,
                 ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF009688),
-                  padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 14.0),
-                  textStyle: TextStyle(fontSize: 16.0),
-                ),
-             
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF009688),
+                padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 14.0),
+                textStyle: TextStyle(fontSize: 16.0),
+              ),
             ),
-            SizedBox(height: 20.0),
+            SizedBox(height: 45.0),
             ElevatedButton(
               onPressed: () {
                 Navigator.pushAndRemoveUntil(
@@ -185,19 +205,151 @@ class UserPage extends StatelessWidget {
                   (route) => false,
                 );
               },
-              child: Text('       Log Out      ',
-              style: TextStyle(
+              child: Text(
+                'Log Out',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 214, 63, 63),
+                padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 14.0),
+                textStyle: TextStyle(fontSize: 16.0),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChangeEmailPage extends StatefulWidget {
+  @override
+  _ChangeEmailPageState createState() => _ChangeEmailPageState();
+}
+
+class _ChangeEmailPageState extends State<ChangeEmailPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  String _emailErrorText = '';
+
+  Future<void> _changeEmail(BuildContext context) async {
+  if (_formKey.currentState!.validate()) {
+    try {
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        AuthCredential credential = EmailAuthProvider.credential(
+          email: user.email!,
+          password: _passwordController.text,
+        );
+        await user.reauthenticateWithCredential(credential);
+        await user.verifyBeforeUpdateEmail(_emailController.text);
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Email Change Requested'),
+              content: Text('A verification email has been sent to your new email address. Please the email to complete the email change process, and log in with the new email.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            LogInPage()), 
+                  );
+                },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } catch (error) {
+      print('Error changing email: $error');
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text('Failed to change email. Please try again.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Change Email'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 20.0),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'New Email',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Email cannot be empty';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Password cannot be empty';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () => _changeEmail(context),
+                child: Text(
+                  'Change Email',
+                  style: TextStyle(
                     color: Colors.white,
                   ),
                 ),
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 217, 62, 62),
+                  backgroundColor: Color(0xFF009688),
                   padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 14.0),
                   textStyle: TextStyle(fontSize: 16.0),
-                  
                 ),
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -242,7 +394,7 @@ class _PasswordResetPageState extends State<PasswordResetPage> {
                     context,
                     MaterialPageRoute(
                         builder: (context) =>
-                            LogInPage()), // Replace LoginPage with your actual login page
+                            LogInPage()), 
                   );
                 },
                 child: Text('OK'),
--- lib/widgets/sensor_connect.dart
+++ lib/widgets/sensor_connect.dart
@@ -11,116 +11,182 @@ class SensorsPage extends StatefulWidget {
 
 class _SensorsPageState extends State<SensorsPage> {
   List<String> availableSensors = [];
+  List<String> userPlant = [];
   String? selectedSensor;
+  String? selectedPlant;
 
   @override
   void initState() {
     super.initState();
     fetchAvailableSensors();
+    fetchUserPlant();
   }
 
   Future<void> fetchAvailableSensors() async {
-    try {
-      QuerySnapshot snapshot = await FirebaseFirestore.instance
-          .collection('sensors')
-          .where('userPlantId', isEqualTo: null)
-          .get();
-
-      List<String> fetchedSensors =
-          snapshot.docs.map((doc) => doc['name'].toString()).toList();
-      setState(() {
-        availableSensors = fetchedSensors;
-      });
-    } catch (error) {
-      print('Error fetching available sensors: $error');
-    }
-  }
-
-  Future<void> connectSensorToPlant(String selectedSensor) async {
+  try {
     String userId = FirebaseAuth.instance.currentUser!.uid;
 
-    try {
-      QuerySnapshot snapshot = await FirebaseFirestore.instance
-          .collection('sensors')
-          .where('name', isEqualTo: selectedSensor)
-          .where('userPlantId', isNotEqualTo: null)
-          .get();
+    QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .collection('sensors')
+        .where('userPlantId', isEqualTo: null)
+        .get();
 
-      if (snapshot.docs.isNotEmpty) {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('Error'),
-              content: Text('The selected sensor is already connected to a plant.'),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text('OK'),
-                ),
-              ],
-            );
-          },
-        );
-        return;
-      }
+    List<String> fetchedSensors = snapshot.docs
+        .map((doc) => doc['name'].toString())
+        .toList();
 
-      // Update sensor document with userPlantId
-      await FirebaseFirestore.instance
-          .collection('sensors')
-          .where('name', isEqualTo: selectedSensor)
-          .limit(1)
-          .get()
-          .then((snapshot) {
-        if (snapshot.docs.isNotEmpty) {
-          String sensorId = snapshot.docs.first.id;
-          FirebaseFirestore.instance
-              .collection('sensors')
-              .doc(sensorId)
-              .update({'userPlantId': userId});
-        }
-      });
-
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Success'),
-            content: Text('Sensor connected to plant successfully.'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text('OK'),
-              ),
-            ],
-          );
-        },
-      );
-    } catch (error) {
-      print('Error connecting sensor to plant: $error');
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Error'),
-            content: Text('Failed to connect sensor to plant. Please try again.'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text('OK'),
-              ),
-            ],
-          );
-        },
-      );
-    }
+    setState(() {
+      availableSensors = fetchedSensors;
+    });
+  } catch (error) {
+    print('Error fetching available sensors: $error');
   }
+}
+
+Future<void> fetchUserPlant() async {
+  try {
+    String userId = FirebaseAuth.instance.currentUser!.uid;
+
+    QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .collection('userPlant')
+        .where('userId', isEqualTo: userId)
+        .where('connected', isEqualTo: false)
+        .get();
+
+    List<String> fetchedPlants = snapshot.docs
+        .map((doc) => doc['plantName'].toString())
+        .toList();
+
+    setState(() {
+      userPlant = fetchedPlants;
+    });
+  } catch (error) {
+    print('Error fetching user plants: $error');
+  }
+}
+
+ Future<void> connectSensorToPlant(String selectedSensor, String selectedPlant) async {
+  String userId = FirebaseAuth.instance.currentUser!.uid;
+
+  try {
+    // Check if the selected sensor is already connected to any plant
+    QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .collection('sensors')
+        .where('name', isEqualTo: selectedSensor)
+        .get();
+
+    if (snapshot.docs.isNotEmpty) {
+      // Iterate over the documents to find if any sensor is already connected to a plant
+      for (QueryDocumentSnapshot doc in snapshot.docs) {
+        // Check if doc.data() is not null before accessing it
+        if (doc.data() != null && (doc.data() as Map<String, dynamic>).containsKey('userPlantId') && doc['userPlantId'] != null) {
+          // If the sensor is already connected, show error message and return
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Error'),
+                content: Text('The selected sensor is already connected to a plant.'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('OK'),
+                  ),
+                ],
+              );
+            },
+          );
+          return;
+        }
+      }
+    }
+
+    // If the execution reaches here, the sensor is not connected to any plant
+    // Update sensor document with userPlantId only if it exists
+    await FirebaseFirestore.instance
+        .collection('sensors')
+        .where('name', isEqualTo: selectedSensor)
+        .limit(1)
+        .get()
+        .then((snapshot) {
+      if (snapshot.docs.isNotEmpty) {
+        String sensorId = snapshot.docs.first.id;
+        Map<String, dynamic> dataToUpdate = {'userPlantId': userId};
+
+        // Check if doc.data() is not null before accessing it
+        if (snapshot.docs.first.data() != null && (snapshot.docs.first.data() as Map<String, dynamic>).containsKey('userPlantId')) {
+          // If the field doesn't exist, remove it from the data to update
+          dataToUpdate.remove('userPlantId');
+        }
+
+        FirebaseFirestore.instance
+            .collection('sensors')
+            .doc(sensorId)
+            .update(dataToUpdate);
+      }
+    });
+
+    // Update userPlant document with connected field
+    await FirebaseFirestore.instance
+        .collection('userPlant')
+        .where('userId', isEqualTo: userId)
+        .where('plantName', isEqualTo: selectedPlant)
+        .limit(1)
+        .get()
+        .then((snapshot) {
+      if (snapshot.docs.isNotEmpty) {
+        String userPlantId = snapshot.docs.first.id;
+        FirebaseFirestore.instance
+            .collection('userPlant')
+            .doc(userPlantId)
+            .update({'connected': true});
+      }
+    });
+
+    // Show success message
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Success'),
+          content: Text('Sensor connected to plant successfully.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  } catch (error) {
+    // Show error message if any error occurs during the process
+    print('Error connecting sensor to plant: $error');
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Error'),
+          content: Text('Failed to connect sensor to plant. Please try again.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -137,7 +203,7 @@ class _SensorsPageState extends State<SensorsPage> {
               'Select a sensor:',
               style: TextStyle(
                 fontSize: 18,
-                fontWeight: FontWeight.bold,
+fontWeight: FontWeight.bold,
               ),
             ),
             SizedBox(height: 8),
@@ -156,10 +222,33 @@ class _SensorsPageState extends State<SensorsPage> {
               }).toList(),
             ),
             SizedBox(height: 16),
+            Text(
+              'Select a plant:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            DropdownButton<String>(
+              value: selectedPlant,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedPlant = newValue;
+                });
+              },
+              items: userPlant.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 16),
             ElevatedButton(
               onPressed: () {
-                if (selectedSensor != null) {
-                  connectSensorToPlant(selectedSensor!);
+                if (selectedSensor != null && selectedPlant != null) {
+                  connectSensorToPlant(selectedSensor!, selectedPlant!);
                 }
               },
               child: Text('Connect'),
